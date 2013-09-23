a56eb90775253044719966d42bac8ee07747cb99
sanjaymeena <sanjaymeena@gmail.com>
2013-09-23 23:00:07 +0800

    Custom fonts in Android

---
+++ ReadMe.txt.txt
@@ -0,0 +1,1 @@
+This Project is related to using custom fonts in Android. 
---
+++ gen/com/sanjaymeena/customfont/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.sanjaymeena.customfont;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
